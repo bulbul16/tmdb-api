d3d12d27ffc6afc406880d59cc94a8e90c3b5610
ahmed <bulbul.cse@outlook.com>
2023-02-25 02:15:53 +0600

    Api endpoint added for user and swagger api documentation added for .net core 2.2

---
+++ tmdb-api.endpoint/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using tmdb_api.domain.service_interfaces;
+using tmdb_api.service;
+
+namespace tmdb_api.endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var result = await _userService.GetUsersAsync();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                throw; 
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var result = await _userService.GetUserByIdAsync(id);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- tmdb-api.endpoint/Startup.cs
+++ tmdb-api.endpoint/Startup.cs
@@ -12,30 +12,51 @@ using System.Threading.Tasks;
 
 namespace tmdb_api.endpoint
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-		}
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+            {
+                Version = "v1",
+                Title = "TMDB API",
+                Description = "This is for API documentation",
+                TermsOfService = "None",
+                Contact = new Swashbuckle.AspNetCore.Swagger.Contact
+                {
+                    Name = "Bulbul Ahmed",
+                    Email="bulbul.cse@outlook.com",
+                    Url= "https://www.grepper.com/profile/bulbul-ahmed"
+                }
+            }));
+        }
 
-			app.UseMvc();
-		}
-	}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TMDB API V1");
+            });
+
+            app.UseMvc();
+        }
+    }
 }
